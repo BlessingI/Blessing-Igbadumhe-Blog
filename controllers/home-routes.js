1037bbdb27d7109fa5af52b59b0892f3980aeac1
Blessing <bigbadumhe@gmail.com>
2022-06-04 14:04:43 -0400

    dashboard according to name

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,7 +34,7 @@ router.get("/", (req, res) => {
     .then((dbPostData) => {
       // pass a single post object into the homepage template
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn, username:req.session.username });
     })
     .catch((err) => {
       console.log(err);
