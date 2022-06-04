e4bc4eab5c5d12adb0f8075e56670ccf2461cdb7
Blessing <bigbadumhe@gmail.com>
2022-06-04 19:24:55 -0400

    killed session after 2 mins

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 120000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
