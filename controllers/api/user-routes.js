9f0978734cc26b1659c99a6aa1563592b87fa33b
Blessing <bigbadumhe@gmail.com>
2022-06-04 13:37:06 -0400

    removed get users route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
 const { User, Post, Comment, Vote } = require("../../models");
 // GET /api/users
-router.get("/", (req, res) => {
-  // Access our User model and run .findAll() method)
-  User.findAll({
-    attributes: { exclude: ["password"] },
-  })
-    .then((dbUserData) => res.json(dbUserData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.get("/", (req, res) => {
+//   // Access our User model and run .findAll() method)
+//   User.findAll({
+//     attributes: { exclude: ["password"] },
+//   })
+//     .then((dbUserData) => res.json(dbUserData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 // GET /api/users/1
 router.get("/:id", (req, res) => {
